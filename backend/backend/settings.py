9be4cc8211bdd1a5fdcfa32b06f06037796c83bb
AlexFeev-S <Staff208397@yandex.ru>
2023-08-14 02:35:38 +0300

    v3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['izitime.sytes.net','127.0.0.1','localhost','51.250.25.191']
+ALLOWED_HOSTS = ['izitime.sytes.net', '127.0.0.1', 'localhost', '51.250.25.191']
 
 
 # Application definition
